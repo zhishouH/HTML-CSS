f1021afa509fb02d0487be29ef7d6f4fd2c6f111
zhishouhuang <zhishouhuang@gmail.com>
2020-06-20 22:14:17 +0800

    flex布局新增

---
+++ MOTOR/index.flex.js
@@ -0,0 +1,9 @@
+var pics = document.querySelector('.evaluation-pics')
+var index = 0
+setInterval (function() {
+    if (index > 3) {
+        index = 0
+    }
+    pics.innerHTML = "<img src='images/pic-lb-" + index + ".jpg'>" + "<img id='hot' src='images/hot.png'>"
+    index++
+},3000)
